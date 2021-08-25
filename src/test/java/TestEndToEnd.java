d958d6943ba1c6bd90ff345a1cb7b7cf46761b32
bryankj97 <bryankkj06@gmail.com>
2021-08-25 13:58:58 +0800

    modify end to end test

--- src/test/java/TestEndToEnd.java
+++ src/test/java/TestEndToEnd.java
@@ -87,4 +87,19 @@ public class TestEndToEnd {
                 App.main(null));
     }
 
+    // illegal canvas dimensions
+    @Test
+    public void testEndtoEnd6() {
+        String userInput = String.format("C 0 4%sL 1 2 6 2%sL 6 3 5 2%sR 14 1 18 3%sB 10 3 o%sQ",
+                System.lineSeparator(),
+                System.lineSeparator(),
+                System.lineSeparator(),
+                System.lineSeparator(),
+                System.lineSeparator());
+        ByteArrayInputStream bais = new ByteArrayInputStream(userInput.getBytes());
+        System.setIn(bais);
+        assertThrows(IllegalDimensionException.class, () ->
+                App.main(null));
+    }
+
 }
